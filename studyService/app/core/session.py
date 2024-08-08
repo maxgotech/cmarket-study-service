64fcc9cc24ac46fe6de2c3ff89456da113055f41
maxgotech <golovinmd@mail.ru>
2024-08-08 20:18:33 +0300

    style: type hints

--- studyService/app/core/session.py
+++ studyService/app/core/session.py
@@ -2,16 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 from typing import Generator
+from sqlalchemy.orm import Session
+from sqlalchemy.engine import Engine
 
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
-print("Database URL is ", SQLALCHEMY_DATABASE_URL)
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
+engine: Engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-def get_db() -> Generator:
+def get_db() -> Generator[Session, None, None]:
     try:
         db = SessionLocal()
         yield db
