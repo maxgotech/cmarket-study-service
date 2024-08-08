d4685d02b57be5e0f976cd45dfe0229b6d63c9d2
maxgotech <golovinmd@mail.ru>
2024-08-08 19:55:03 +0300

    feat: db config

---
+++ studyService/app/core/config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+from pathlib import Path
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+class Settings:
+    PROJECT_NAME: str = "CMARKET-STUDY-SERVICE"
+    PROJECT_VERSION: str = "1.0.0"
+
+    DATABASE_URL = os.environ["DATABASE_URI"]
+
+
+settings = Settings()
