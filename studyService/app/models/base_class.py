2023f886ce7d2ed38dbc13403cab59f69ab2e66d
maxgotech <golovinmd@mail.ru>
2024-08-09 19:37:47 +0300

    fear: removed update, added dict methods

--- studyService/app/models/base_class.py
+++ studyService/app/models/base_class.py
@@ -1,8 +1,8 @@
 from sqlalchemy.orm import DeclarativeBase
+from typing import Any, Dict
+from sqlalchemy import inspect
+
 
 class Base(DeclarativeBase):
-    
-    def update(self, **kwargs):
-        for key, value in kwargs.items():
-            if hasattr(self, key):
-                setattr(self, key, value)
+    def dict(self) -> Dict[str, Any]:
+        return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
