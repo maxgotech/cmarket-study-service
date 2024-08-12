84d26ac1bc49c1e4a823e426daa0d5a58a1c1762
maxgotech <golovinmd@mail.ru>
2024-08-12 18:40:19 +0300

    revert: "perf: uvloop event loop policy"
    This reverts commit 1b2e25281e83c1122be1607d4e19ecbf457f0637.

--- studyService/app/main.py
+++ studyService/app/main.py
@@ -1,14 +1,8 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from app.api import router
 from app.core.config import settings
 from app.core.session import init_db
 from contextlib import asynccontextmanager
-import time
-
-import asyncio
-import uvloop
-
-asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 
 @asynccontextmanager
@@ -17,7 +11,6 @@ async def lifespan(app: FastAPI):
     await init_db()
     yield
 
-
 # init app
 app = FastAPI(
     title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION, lifespan=lifespan
@@ -25,16 +18,3 @@ app = FastAPI(
 
 # add router to app
 app.include_router(router)
-
-
-# logger for request timing
-@app.middleware("http")
-async def add_process_time_header(request: Request, call_next):
-    start_time = time.time()
-    response = await call_next(request)
-    print(
-        "Time took to process the request and return response is {} sec".format(
-            time.time() - start_time
-        )
-    )
-    return response
