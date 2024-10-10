93825bfe191b0498aba0052e122794799f41e80b
maxgotech <golovinmd@mail.ru>
2024-10-10 17:43:28 +0300

    fix: exception that never fires

--- studyService/app/api/v1/studies.py
+++ studyService/app/api/v1/studies.py
@@ -97,12 +97,6 @@ async def update_study(study_in: StudyUpdate, db: AsyncSession = Depends(get_db)
     Update study
     """
 
-    # if no id then we cant find the
-    if study_in.id == None:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid body request"
-        )
-
     # get study will raise 404 if no study is not found
     await get_study(study_in.id, db)
 
