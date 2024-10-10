9b7fa5282edba38e0e825954a7b1d5c6734a50d4
maxgotech <golovinmd@mail.ru>
2024-10-10 18:53:51 +0300

    test: update handler tests

--- studyService/tests/test_v1_studies.py
+++ studyService/tests/test_v1_studies.py
@@ -8,7 +8,7 @@ from fastapi import status
 from unittest.mock import patch, AsyncMock
 from app.models.studies import StudyModel
 from typing import Any, Optional
-from app.schemas.study import StudyCreate
+from app.schemas.study import StudyCreate, StudyUpdate
 
 
 @dataclass
@@ -18,6 +18,7 @@ class Case:
     values: Any
     status: int
     user: Optional[int] = None
+    addon: Any = None
 
 
 @pytest.mark.anyio
@@ -77,8 +78,8 @@ async def test_create_study(async_client):
             name="USER NOT FOUND",
             data=StudyCreate(name="Study", userid=1),
             values=StudyModel(id=1, name="Study", study_order=1, userid=1),
-            status=status.HTTP_404_NOT_FOUND,
             user=0,
+            status=status.HTTP_404_NOT_FOUND,
         ),
         Case(
             name="OK",
@@ -91,8 +92,8 @@ async def test_create_study(async_client):
             name="RANDOM INPUT",
             data=StudyModel(name="Study", study_order=1),
             values=StudyModel(id=1, name="Study", study_order=1, userid=4),
-            status=status.HTTP_422_UNPROCESSABLE_ENTITY,
             user=4,
+            status=status.HTTP_422_UNPROCESSABLE_ENTITY,
         ),
     ]
     with (
@@ -104,6 +105,59 @@ async def test_create_study(async_client):
             mock_crud.create = AsyncMock(return_value=tc.values)
             user_mock.return_value = tc.user
             res = await async_client.post(
+                "/api/v1/studies/",
+                data=json.dumps(tc.data.dict()),
+            )
+            assert res.status_code == tc.status, tc.name
+
+
+@pytest.mark.anyio
+async def test_update_study(async_client):
+    cases: list[Case] = [
+        Case(
+            name="INVALID REQUEST BODY",
+            data=StudyCreate(name="Study", userid=1),
+            values=None,
+            addon=StudyModel(id=1, name="Study", study_order=1, userid=1),
+            status=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        ),
+        Case(
+            name="STUDY NOT FOUND",
+            data=StudyUpdate(
+                id=4,
+                name="New name",
+                type_content=None,
+                id_content=None,
+                courseid=None,
+                moduleid=None,
+            ),
+            values=StudyModel(id=1, name="Study", study_order=1, userid=1),
+            addon=None,
+            status=status.HTTP_404_NOT_FOUND,
+        ),
+        Case(
+            name="OK",
+            data=StudyUpdate(
+                id=4,
+                name="New name",
+                type_content=None,
+                id_content=None,
+                courseid=None,
+                moduleid=None,
+            ),
+            values=StudyModel(id=1, name="Study", study_order=1, userid=4),
+            addon=StudyModel(id=1, name="Study", study_order=1, userid=1),
+            status=status.HTTP_200_OK,
+        ),
+    ]
+    with (
+        patch("app.api.v1.studies.crud_study") as mock_crud,
+        patch("app.api.v1.studies.send_one", AsyncMock(return_value=None)),
+    ):
+        for tc in cases:
+            mock_crud.update = AsyncMock(return_value=tc.values)
+            mock_crud.get = AsyncMock(return_value=tc.addon)
+            res = await async_client.patch(
                 "/api/v1/studies/",
                 data=json.dumps(tc.data.dict()),
             )
