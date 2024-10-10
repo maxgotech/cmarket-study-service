84399753cc53e73ae33c66e190f1ce7c3376220c
maxgotech <golovinmd@mail.ru>
2024-10-10 15:17:06 +0300

    test: health updated test

--- studyService/tests/test_health.py
+++ studyService/tests/test_health.py
@@ -1,9 +1,11 @@
 import pytest
 from fastapi import status
 from httpx import AsyncClient
+from unittest.mock import patch, AsyncMock
+from tests.conftest import MockDb
 
 
 @pytest.mark.anyio
-async def test_health(async_client: AsyncClient):
+async def test_health(db: MockDb, async_client: AsyncClient):
     res = await async_client.get("/api/health/")
     assert res.status_code == status.HTTP_204_NO_CONTENT
